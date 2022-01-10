c4349b30755d325f3639551144d3f27d0267d5db
YelyzavetaLukianova <yelyzaveta.lukianova@gmail.com>
2022-01-10 19:07:01 +0300

    hw7thr

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -17,7 +17,6 @@ const ContactForm = ({ addNewContact }) => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    dispatch(addContact({ name, number, id: nanoid(10) }));
     addNewContact({ name, number, id: nanoid(10) });
     reset();
   };
